feffe91860169b0c4959979e789de5b109c5831d
Portik Ábel <portikusz0@gmail.com>
2021-03-30 18:25:23 +0300

    button events

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const r = 250;
-const angle = Math.PI / 10;
+const r = 500;
+const angle = Math.PI / 40;
 
 const colors = [
   '#404E4D',
@@ -15,20 +15,26 @@ const colors = [
 ];
 const buttons = document.querySelector('#buttons');
 
-let lines, sticks;
+let lines;
 
 function initializeSticks() {
   lines = [];
   for (let i = 0; i < 10; i++) {
     let p1 = {
-      x: r * (1 + Math.sin(angle * 2 * i)),
-      y: r * (1 + Math.cos(angle * 2 * i)),
+      x: r * (1 - Math.sin(angle * i)),
+      y: r * (1 - Math.cos(angle * i)),
     };
     let p2 = {
-      x: r * (1 + Math.sin(angle * (2 * i + 9))),
-      y: r * (1 + Math.cos(angle * (2 * i + 9))),
+      x: r * (1 - Math.sin(angle * (i + 10))),
+      y: r * (1 - Math.cos(angle * (i + 10))),
     }
-    lines.push({start: p1, end: p2, color: colors[i]});
+    lines.push({
+        index: i, 
+        start: p1, 
+        end: p2, 
+        color: colors[i], 
+        visible: true,
+      });
   }
 }
 
@@ -36,17 +42,18 @@ function randomize(list){
   let newList = [];
   while(list.length > 0) {
     const index = Math.floor(Math.random() * list.length);
-    newList.push({ind: index, element: list[index]});
+    newList.push(list[index]);
     list.splice(index, 1);
   }
   return newList;
 }
 
 function paintSticks(sticks) {
-  let ctx = document.querySelector('#game-board').getContext('2d');
+  const canvas = document.querySelector('#game-board');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.lineWidth = 5;
-  sticks.forEach(stick => {
-    let line = stick.element;
+  sticks.filter(x => x.visible).forEach(line => {
     ctx.beginPath();
     ctx.strokeStyle = line.color;
     ctx.moveTo(line.start.x, line.start.y);
@@ -55,11 +62,19 @@ function paintSticks(sticks) {
   }); 
 }
 
+function btnClicked(event) {
+  let i = event.target.id;
+  lines.find(line => line.index == i).visible = false;
+  paintSticks(lines);
+}
+
 function addButtons(sticks) {
   sticks.forEach(line => {
     const button = document.createElement('button');
     button.className = 'colored-button';
+    button.id = line.index;
     button.style.backgroundColor = line.color;
+    button.onclick = btnClicked;
     buttons.appendChild(button);
   }); 
 }
@@ -67,14 +82,14 @@ function addButtons(sticks) {
 function load() {
   initializeSticks();
   addButtons(lines);
-  sticks = randomize(lines);
-  paintSticks(sticks);
+  lines = randomize(lines);
+  paintSticks(lines);
 }
 
 function reload() {
   initializeSticks();
-  sticks = randomize(lines);
-  paintSticks(sticks);
+  lines = randomize(lines);
+  paintSticks(lines);
 }
 
 window.onload = load;
