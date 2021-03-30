e2a89f6fa5bba8bdde277ea4118c6ada21b90a94
Portik Ábel <portikusz0@gmail.com>
2021-03-30 10:33:48 +0300

    validation+footer

---
+++ validate.js
@@ -0,0 +1,57 @@
+const gmailPattern = /^[a-zA-Z.!#$%&'*+\/=?^_`{|}~-]+@gmail.[a-z]+$/;
+const yahooPattern = /^[a-zA-Z.!#$%&'*+\/=?^_`{|}~-]+@yahoo.[a-z]+$/;
+
+function validateEmail() {
+  const emailInput = document.forms['registerForm']['email'];
+  if(emailInput.value.match(gmailPattern) || 
+    emailInput.value.match(yahooPattern)) {
+    emailInput.setCustomValidity('');
+    emailInput.validity.valid = true;
+  }
+  else {
+    emailInput.setCustomValidity(
+      'Expecting valid gmail/yahoo e-mail address.'
+    );
+    emailInput.validity.valid = false;
+  }
+}
+
+const urlPattern = /www.[a-zA-Z0-9_\-\/.]+/
+
+function validateUrl() {
+  const urlInput = document.forms['registerForm']['web-adrs'];
+  if(urlInput.value.match(urlPattern)) {
+    urlInput.setCustomValidity('');
+  }
+  else {
+    urlInput.setCustomValidity('Invalid url.');
+  }
+}
+
+const fields = document.querySelectorAll('.input');
+const submitBtn = document.querySelector('.button');
+submitBtn.disabled = true;
+submitBtn.style.cursor = 'not-allowed';
+
+function validate() {
+  submitBtn.disabled = false;
+  fields.forEach(field => {
+      submitBtn.disabled |= !field.validity.valid;
+    }
+  );
+  submitBtn.style.cursor = submitBtn.disabled ? 'not-allowed' : 'pointer'
+}
+
+fields.forEach(field => {
+    field.addEventListener('input', validate);
+  }
+);
+
+let modifDate = "Last modified: " + document.lastModified;
+
+function updateLastModified() {
+  modifDate = "Last modified: " + document.lastModified;
+}
+
+const footer = document.querySelector("footer");
+footer.innerText = modifDate;
