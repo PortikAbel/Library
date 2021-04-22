4d9ec862430134cf4c62f346151aa745fb950623
Portik Ábel <portikusz0@gmail.com>
2021-04-22 11:24:28 +0300

    mongo

--- routes/books.js
+++ routes/books.js
@@ -18,34 +18,62 @@ router.post('/register', (req, res) => {
     imageName: coverImgHandler.name,
   };
 
-  mongo.MongoClient.connect(url, (err, db) => {
+  mongo.MongoClient.connect(url, (err, client) => {
     if (err) throw err;
-    const dbo = db.db('library');
-    console.log('Connected to db library');
-    dbo.collection('books').insertOne(newBook, (insertErr) => {
-      if (insertErr) throw insertErr;
-      console.log('1 document inserted');
-      db.close();
-    });
+    const books = client.db('library').collection('books');
+    books.insertOne(newBook);
+    client.close();
   });
 
   res.set({
     'Content-Type': 'text/plain',
   });
+  res.end(`Book registered ${JSON.stringify(newBook)}`);
 });
 
 router.post('/rent', (req, res) => {
-  const respBody = `Book rent:
-    username: ${req.fields.username}
-    isbn: ${req.fields.isbn}
-  `;
-
-  console.log(respBody);
   res.set({
     'Content-Type': 'text/plain',
   });
-  res.end(respBody);
+
+  mongo.MongoClient.connect(url)
+    .then((client) => {
+      const dbo = client.db('library');
+      const querry = { _id: req.fields.isbn };
+      const dec = { $inc: { copies: -1 } };
+      const rent = {
+        renter: req.fields.username,
+        isbn: req.fields.isbn,
+      };
+      dbo.collection('books')
+        .findOne(querry)
+        .then((result) => {
+          console.log(result);
+          if (result === null || result.copies <= 0) {
+            Promise.reject(new Error(
+              `Book with ISBN ${req.fields.isbn} not found in our library right now.`,
+            ));
+          }
+          Promise.resolve(
+            dbo.collection('books')
+              .updateOne(querry, dec),
+          );
+        })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => req.status(400).send(error.message));
+      client.close();
+    });
 });
+   /*
+        dbo.collection('rents')
+          .insertOne(rent)
+          .catch(console.log('error occured at inserting rent'));
+        res.end(
+          `User ${req.fields.username} rented book with isbn ${req.fields.isbn} succesfully`,
+        );
+      }*/
 
 router.post('/return', (req, res) => {
   const respBody = `Book return:
