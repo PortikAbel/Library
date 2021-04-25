383edc92de19d3ec28ad56bc370a6b824fdcf9ec
Portik Ábel <portikusz0@gmail.com>
2021-04-25 23:45:35 +0300

    javitva

--- routes/books.js
+++ routes/books.js
@@ -18,13 +18,10 @@ router.post('/register', (req, res) => {
     'Content-Type': 'text/plain',
   });
 
-  try {
-    fs.rename(
-      coverImgHandler.path,
-      path.join(process.cwd(), 'books', coverImgHandler.name),
-      (err) => { if (err) throw err; },
-    );
-
+  fs.promises.rename(
+    coverImgHandler.path,
+    path.join(process.cwd(), 'books', coverImgHandler.name),
+  ).then(() => {
     const newBook = {
       _id: parseInt(req.fields.isbn, 10),
       title: req.fields.title,
@@ -34,18 +31,18 @@ router.post('/register', (req, res) => {
       copies: parseInt(req.fields.copies, 10),
       imageName: coverImgHandler.name,
     };
-
     const { error } = registerSceme.validate(newBook);
-    if (error) {
+    if (error != null) {
       throw new Error(error);
     }
-
+    return newBook;
+  }).then((newBook) => {
     const books = library.collection('books');
-    books.insertOne(newBook);
-    res.send(`Book registered ${JSON.stringify(newBook)}`);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
+    return books.insertOne(newBook);
+  }).then((result) => {
+    res.send(`Book registered ${JSON.stringify(result.ops)}`);
+  })
+    .catch((err) => res.status(400).send(err.message));
 });
 
 router.post('/rent', (req, res) => {
@@ -53,39 +50,35 @@ router.post('/rent', (req, res) => {
     'Content-Type': 'text/plain',
   });
 
-  try {
-    const querry = { _id: parseInt(req.fields.isbn, 10) };
-    const dec = { $inc: { copies: -1 } };
-    const rent = {
-      renter: req.fields.username,
-      isbn: parseInt(req.fields.isbn, 10),
-    };
+  const querry = { _id: parseInt(req.fields.isbn, 10) };
+  const dec = { $inc: { copies: -1 } };
+  const rent = {
+    renter: req.fields.username,
+    isbn: parseInt(req.fields.isbn, 10),
+  };
 
-    const { error } = rentScheme.validate(rent);
-    if (error) {
-      throw new Error(error);
-    }
-
-    library.collection('books')
-      .findOne(querry)
-      .then((result) => {
-        if (result === null || result.copies <= 0) {
-          throw new Error(
-            `Book with ISBN ${req.fields.isbn} not found in our library right now.`,
-          );
-        }
-      })
-      .then(() => {
-        library.collection('books').updateOne(querry, dec);
-      })
-      .then(() => library.collection('rents').insertOne(rent))
-      .then(() =>  res.end(
-        `User ${req.fields.username} rented book with isbn ${req.fields.isbn} succesfully`,
-      ))
-      .catch((err) => res.status(400).send(err.message));
-  } catch (err) {
-    res.status(400).send(err.message);
+  const { error } = rentScheme.validate(rent);
+  if (error != null) {
+    res.status(400).send(error);
+    return;
   }
+
+  library.collection('books').findOne(querry)
+    .then((result) => {
+      if (result === null || result.copies <= 0) {
+        throw new Error(
+          `Book with ISBN ${req.fields.isbn} not found in our library right now.`,
+        );
+      }
+    })
+    .then(() => {
+      library.collection('books').updateOne(querry, dec);
+    })
+    .then(() => library.collection('rents').insertOne(rent))
+    .then(() =>  res.end(
+      `User ${req.fields.username} rented book with isbn ${req.fields.isbn} succesfully`,
+    ))
+    .catch((err) => res.status(400).send(err.message));
 });
 
 router.post('/return', (req, res) => {
@@ -93,38 +86,36 @@ router.post('/return', (req, res) => {
     'Content-Type': 'text/plain',
   });
 
-  try {
-    const bookQuerry = { _id: parseInt(req.fields.isbn, 10) };
-    const rentQuerry = {
-      renter: req.fields.username,
-      isbn: parseInt(req.fields.isbn, 10),
-    };
-    const inc = { $inc: { copies: 1 } };
+  const bookQuerry = { _id: parseInt(req.fields.isbn, 10) };
+  const rentQuerry = {
+    renter: req.fields.username,
+    isbn: parseInt(req.fields.isbn, 10),
+  };
+  const inc = { $inc: { copies: 1 } };
 
-    const { error } = rentScheme.validate(rentQuerry);
-    if (error) {
-      throw new Error(error);
-    }
-
-    library.collection('rents')
-      .deleteOne(rentQuerry)
-      .then((result) => {
-        if (result.deletedCount < 1) {
-          throw new Error(`User ${req.fields.username} has not rented book wit ISBN ${req.fields.isbn}`);
-        }
-        library.collection('books').updateOne(bookQuerry, inc);
-      })
-      .then(() => res.send(`User ${req.fields.username} succesfully renturned book wit ISBN ${req.fields.isbn}`))
-      .catch((err) => res.status(400).send(err.message));
-  } catch (err) {
-    res.status(400).send(err.message);
+  const { error } = rentScheme.validate(rentQuerry);
+  if (error != null) {
+    res.status(400).send(error);
+    return;
   }
+  library.collection('rents').deleteOne(rentQuerry)
+    .then((result) => {
+      if (result.deletedCount < 1) {
+        throw new Error(`User ${req.fields.username} has not rented book wit ISBN ${req.fields.isbn}`);
+      }
+      library.collection('books').updateOne(bookQuerry, inc);
+    })
+    .then(() => res.send(`User ${req.fields.username} succesfully renturned book wit ISBN ${req.fields.isbn}`))
+    .catch((err) => res.status(400).send(err.message));
 });
 
 export default router;
 
 export async function connectDb() {
-  library = await dbo.connect()
-    .then((db) =>  db.db('library'))
-    .catch(() => console.log('Could not connect to database.'));
+  try {
+    library = await dbo.connect();
+    library = library.db('library');
+  } catch (err) {
+    console.log('Could not connect to database.');
+  }
 }
